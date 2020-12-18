062eea7135290cb84c0f01794fd9d88a714a6f36
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-18 18:31:27 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_validation/src/page/home_page.dart';
+import 'package:flutter_form_validation/src/page/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Form Validation',
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (BuildContext context) => HomePage(),
+        LoginPage.routeName: (BuildContext context) => LoginPage(),
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Center(
+          child: Container(
+              child: Text('Pon_aqui_tu_titulo')
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/page/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  static final String routeName = 'login_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Login'),
+      ),
+    );
+  }
+}
