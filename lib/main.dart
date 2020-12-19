4d452c2b78f795c08ac9d148bf7cb7d8fb4f261b
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-19 08:07:48 +0100

    [FEATURE][PROVIDER] * InheritedWidget was implemented to notify to all children that are any change into email and password textfields.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_validation/src/blocs/provider.dart';
 import 'package:flutter_form_validation/src/page/home_page.dart';
 import 'package:flutter_form_validation/src/page/login_page.dart';
 
@@ -9,21 +10,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Form Validation',
-      initialRoute: LoginPage.routeName,
-      routes: {
-        HomePage.routeName: (BuildContext context) => HomePage(),
-        LoginPage.routeName: (BuildContext context) => LoginPage(),
-      },
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
-        ),
-        body: Center(
-          child: Container(
-              child: Text('Pon_aqui_tu_titulo')
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Form Validation',
+        initialRoute: LoginPage.routeName,
+        routes: {
+          HomePage.routeName: (BuildContext context) => HomePage(),
+          LoginPage.routeName: (BuildContext context) => LoginPage(),
+        },
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Login'),
+          ),
+          body: Center(
+            child: Container(child: Text('Pon_aqui_tu_titulo')),
           ),
         ),
       ),
