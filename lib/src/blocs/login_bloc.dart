ca334c58a39dac3e3a3b341beef033c64301cdc8
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-19 07:55:38 +0100

    [FEATURE][STREAM] * Stream class was implemented to listen email and password textfields

---
+++ lib/src/blocs/login_bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+class LoginBloc {
+  // 2 controladores uno para estar observando el email y otro para observar el password
+  /*
+   Se le agrega la informacion de String al Stream controller para poder escuchar mas elementos a los controladores, en caso de dejar los controladores solo como StreamController(), solo podria tener
+   un observador, sin embargo poniendo el .broadcast() permitimos que pueda haber mas de un observador de estos controladores.
+   */
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
+
+  Stream<String> get emailStream => _emailController.stream;
+  Stream<String> get passwordStream => _passwordController.stream;
+
+  // Inserción de valores al stream, aunque ponga get son setters insercion de string en los campos
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose(){
+    _emailController?.close();
+    _passwordController?.close();
+  }
+}
