db4e531652bb6ad62c5b6d75b1bc962258db081b
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-19 11:53:56 +0100

    [FEATURE][PROVIDER] * Get products from firebase was added

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_validation/src/models/product_model.dart';
 import 'package:flutter_form_validation/src/page/product_page.dart';
+import 'package:flutter_form_validation/src/providers/products_provider.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = 'home_page';
+  final productsProvider = ProductsProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +13,29 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('HOME'),
       ),
-      body: Container(),
+      body: _buildProductList(),
       floatingActionButton: _floatingActionButtonBuilder(context),
     );
   }
-  
+
   Widget _floatingActionButtonBuilder(BuildContext context) {
     return FloatingActionButton(
-      backgroundColor: Colors.deepPurple,
-      child: Icon(Icons.add),
-        onPressed: () => Navigator.pushNamed(context, ProductPage.routeName)
-        );
+        backgroundColor: Colors.deepPurple,
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.pushNamed(context, ProductPage.routeName));
+  }
+
+  Widget _buildProductList() {
+    return FutureBuilder(
+        future: productsProvider.getProducts(),
+        builder: (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
+          if (snapshot.hasData) {
+            return Container();
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
--- lib/src/providers/products_provider.dart
+++ lib/src/providers/products_provider.dart
@@ -1,18 +1,35 @@
 import 'dart:convert';
 import 'package:flutter_form_validation/src/models/product_model.dart';
 import 'package:http/http.dart' as http;
+
 class ProductsProvider {
   final String _firebaseBaseURL = 'YOUR_FIREBASE_URL';
 
   Future<bool> postProduct(ProductModel productModel) async {
     final url = '$_firebaseBaseURL/products.json';
     final response = await http.post(url, body: productModelToJson(productModel));
-    
     final decodedData = json.decode(response.body);
-
     print(decodedData);
-
     return true;
   }
 
+  Future<List<ProductModel>> getProducts() async {
+    final url = '$_firebaseBaseURL/products.json';
+    final response = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    final List<ProductModel> products = List();
+
+    if (decodedData == null) {
+      return [];
+    } else {
+      decodedData.forEach((id, product) {
+        final tempProduct = ProductModel.fromJson(product);
+        tempProduct.id = id;
+        products.add(tempProduct);
+      });
+      return products;
+    }
+  }
 }
