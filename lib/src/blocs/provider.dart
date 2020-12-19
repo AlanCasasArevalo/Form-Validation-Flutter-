5d2dc0e62125df9d958daf924fc61e853832d00f
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-19 09:33:26 +0100

    [FEATURE][LOGIN] * Some change were done if you do a hot reload.

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -3,9 +3,20 @@ import 'package:flutter_form_validation/src/blocs/login_bloc.dart';
 export 'package:flutter_form_validation/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget {
+
+  static Provider _instance;
+
+  factory Provider({Key key, Widget child}) {
+    if(_instance == null) {
+      _instance = Provider._internal(key: key, child: child,);
+    }
+    return _instance;
+  }
+
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
+
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
