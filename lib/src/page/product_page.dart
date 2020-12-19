d418429d28048bd846cfd4be819d035f7275cc69
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-19 11:02:02 +0100

    [FEATURE][PRODUCT FORM] * Product form was improved with our product model

--- lib/src/page/product_page.dart
+++ lib/src/page/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_validation/src/models/product_model.dart';
 import 'package:flutter_form_validation/src/utils/utils.dart' as utils;
 
 class ProductPage extends StatefulWidget {
@@ -11,13 +12,16 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   final formKey = GlobalKey<FormState>();
 
+  ProductModel _productModel = ProductModel();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Producto'),
         actions: [
-          IconButton(icon: Icon(Icons.photo_size_select_actual), onPressed: () {}),
+          IconButton(
+              icon: Icon(Icons.photo_size_select_actual), onPressed: () {}),
           IconButton(icon: Icon(Icons.camera_alt), onPressed: () {}),
         ],
       ),
@@ -29,9 +33,17 @@ class _ProductPageState extends State<ProductPage> {
             child: Column(
               children: [
                 _productNameBuilder(),
-                SizedBox(height: 16,),
+                SizedBox(
+                  height: 16,
+                ),
                 _productPriceBuilder(),
-                SizedBox(height: 16,),
+                SizedBox(
+                  height: 16,
+                ),
+                _availableBuilder(),
+                SizedBox(
+                  height: 16,
+                ),
                 _submitButtonBuilder()
               ],
             ),
@@ -42,11 +54,10 @@ class _ProductPageState extends State<ProductPage> {
   }
 
   TextFormField _productNameBuilder() {
-    return TextFormField (
+    return TextFormField(
+      initialValue: _productModel.name,
       textCapitalization: TextCapitalization.sentences,
-      decoration: InputDecoration(
-        labelText: 'Producto'
-      ),
+      decoration: InputDecoration(labelText: 'Producto'),
       validator: (value) {
         if (value.length < 3) {
           return 'Ingrese el nombre del producto completo';
@@ -54,40 +65,54 @@ class _ProductPageState extends State<ProductPage> {
           return null;
         }
       },
+      onSaved: (value) {
+        _productModel.name = value;
+      },
+    );
+  }
+
+  Widget _availableBuilder() {
+    return SwitchListTile(
+      activeColor: Colors.deepPurple,
+      title: Text('Disponible'),
+        value: _productModel.available,
+        onChanged: (value) => setState(() {
+              _productModel.available = value;
+            }
+        )
     );
   }
 
   TextFormField _productPriceBuilder() {
-    return TextFormField (
+    return TextFormField(
+      initialValue: _productModel.price.toString(),
       keyboardType: TextInputType.numberWithOptions(decimal: true),
-      decoration: InputDecoration(
-          labelText: 'Precio'
-      ),
+      decoration: InputDecoration(labelText: 'Precio'),
       validator: (value) {
-        if(utils.isNumber(value)) {
+        if (utils.isNumber(value)) {
           return null;
         } else {
           return 'Solo puede poner números.';
         }
+      },
+      onSaved: (value) {
+        _productModel.price = double.parse(value);
       },
     );
   }
 
   RaisedButton _submitButtonBuilder() {
     return RaisedButton.icon(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20)
-      ),
-      color: Colors.deepPurple,
-      textColor: Colors.white,
-      icon: Icon(Icons.save),
-      label: Text('Guardar'),
-        onPressed: _submit
-        );
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: Colors.deepPurple,
+        textColor: Colors.white,
+        icon: Icon(Icons.save),
+        label: Text('Guardar'),
+        onPressed: _submit);
   }
 
   void _submit() {
     if (!formKey.currentState.validate()) return;
-    print('Todo bien');
+    formKey.currentState.save();
   }
 }
